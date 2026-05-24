673f62570ea1a5bbca928efc83e6f4394c6ca2eb
Mohammad Arman <khanmangilal35@gmail.com>
2026-05-24 12:56:20 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,3 +1,4 @@
+#------------------Day 1 practis code
 def symballs():
     symball = input("Enter a symball : ")
     for i in range(10, 0, -1):
@@ -5,3 +6,19 @@ def symballs():
 
 # without symball call function
 symballs()
+
+
+#----------------------------------------------------
+#----------Day2 practise code---------
+#----Dat2 --function whith calculator code-------
+def jarvish ():
+	num1=int(input("Enter a num :"))
+	num2=int(input("Enter a nym :"))
+	print(num1+num2)
+	print(num1- num2)
+	print(num2- num1)
+	print(num1* num2)
+	print(num1/num2)
+	print(num2/num1)
+	
+jarvish()
