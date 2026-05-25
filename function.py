6d7503e4d3ce754e94a1d055376311ee9daf9623
Mohammad Arman <khanmangilal35@gmail.com>
2026-05-25 15:23:12 +0530

    Update function.py

--- function.py
+++ function.py
@@ -22,3 +22,50 @@ def jarvish ():
 	print(num2/num1)
 	
 jarvish()
+
+#----------------------------------------
+#------Day 3 practise code---------------
+#---Day 3 function in loos & conditional statement and methods in calculator-----
+def jarvish():
+    print("\n⚡ Jarvis System Ready ⚡")
+    
+    # Error Handling: कोड क्रैश होने से बचाने के लिए
+    try:
+        num1 = int(input("Enter first num: "))
+        num2 = int(input("Enter second num: "))
+        
+        # सारे कैलकुलेशन एक साथ
+        print(f"➕ Addition:       {num1 + num2}")
+        print(f"➖ Subtraction 1:  {num1 - num2}")
+        print(f"➖ Subtraction 2:  {num2 - num1}")
+        print(f"✖️ Multiplication: {num1 * num2}")
+        print(f"POVER == {num1**num2}")
+        # Zero से भाग (Divide) होने से बचाना
+        if num2 != 0:
+            print(f"➗ Division 1:     {num1 / num2}")
+        else:
+            num2 +=1
+            print(f"Division1 : {num1/num2}")
+        print("Division1 sucsessfull")
+            
+        if num1 != 0:
+            print(f"➗ Division 2:     {num2 / num1}")
+        else:
+            num1 +=1
+            print(f"Division2 : {num2/num1}")
+        print("Division2 sucsessfull")
+            
+    except ValueError:
+        print("❌ Error: कृपया सिर्फ नंबर्स (1, 2, 3) ही डालें, एबीसीडी नहीं!")
+
+# MAIN LOOP: जो प्रोग्राम को लगातार चलाएगा
+while True:
+    jarvish()
+    
+    # यूजर से पूछना कि आगे चलाना है या बंद करना है
+    choice = input("\nक्या आप फिर से चलाना चाहते हैं? (yes/no): ").lower()
+    if choice == 'no':
+        print("👋 Goodbye! Jarvis shutting down.")
+        break  # लूप को तुरंत रोक देगा
+
+
