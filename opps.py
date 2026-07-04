e92f4004224bd6cbc6f85e2385378e8177ca03bf
Mohammad Arman <khanmangilal35@gmail.com>
2026-07-04 15:44:33 +0530

    Add files via upload

---
+++ opps.py
@@ -0,0 +1,14 @@
+#=====≈==================
+#=====_my_first_program_for_opps_======
+class student:
+	def __init__(self,name,marks):
+		 self.name=name
+		 self.marks=marks
+		 
+		
+s1=student("arman",90.17)
+s2=student("rahul",87.9)
+print(s1.name ,s1.marks )
+print(s2.name ,s2.marks )
+
+	
